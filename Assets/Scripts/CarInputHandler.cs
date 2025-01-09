332bcea3938c92f6581937d63263b0be5785f774
Ali Farhat <ackquwa@gmail.com>
2025-01-10 01:10:08 +0400

    Added Gamepad Input and Individial Steering per player.
    P1 is WASD, P2 is Arrow Keys, and P3 is Gamepad DS4 Controller.

--- Assets/Scripts/CarInputHandler.cs
+++ Assets/Scripts/CarInputHandler.cs
@@ -4,20 +4,54 @@ using UnityEngine;
 
 public class CarInputHandler : MonoBehaviour
 {
+    public int playerNumber = 1;
+
     //Components
     TopDownCarController topDownCarController;
 
+    //Awake is called when the script instance is being loaded.
     void Awake()
     {
         topDownCarController = GetComponent<TopDownCarController>();
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame and is frame dependent
     void Update()
     {
         Vector2 inputVector = Vector2.zero;
 
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+        switch (playerNumber)
+        {
+            case 1:
+                //Get input from Unity's input system.
+                inputVector.x = Input.GetAxis("Horizontal_P1");
+                inputVector.y = Input.GetAxis("Vertical_P1");
+                break;
+
+            case 2:
+                //Get input from Unity's input system.
+                inputVector.x = Input.GetAxis("Horizontal_P2");
+                inputVector.y = Input.GetAxis("Vertical_P2");
+                break;
+
+            case 3:
+                //Get input from Unity's input system.
+                inputVector.x = Input.GetAxis("Horizontal_P3_DS4Controller");
+                inputVector.y = Input.GetAxis("Vertical_P3_DS4Controller");
+                break;
+
+            case 4:
+                //Get input from Unity's input system.
+                inputVector.x = Input.GetAxis("Horizontal_P4");
+                inputVector.y = Input.GetAxis("Vertical_P4");
+                break;
+        }
 
         //Send the input to the car controller.
         topDownCarController.SetInputVector(inputVector);
